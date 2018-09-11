c25afb7301c99b88cc84df6ea4cccde96907d8b1
Michael McLoughlin <mmcloughlin@gmail.com>
2018-09-11 09:06:23 +0100

    more functions in example

--- example.cc
+++ example.cc
@@ -1,5 +1,11 @@
 #include <iostream>
 
-__attribute__((annotate("foo"))) int add(int a, int b) { return a + b; }
+int add(int a, int b) { return a + b; }
+
+__attribute__((annotate("foo"))) void annotated(void) {
+  std::cout << "Hello from foo!" << std::endl;
+}
+
+void empty(void) {}
 
 int main(void) { std::cout << add(2, 3) << std::endl; }
