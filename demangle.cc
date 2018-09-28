0621b361eda49c1b63d2781adb9b70373656fa13
Michael McLoughlin <mmcloughlin@gmail.com>
2018-09-27 20:40:25 -0700

    use partialdemangler

--- demangle.cc
+++ demangle.cc
@@ -7,29 +7,51 @@ using namespace llvm;
 
 namespace {
 
-std::string DemangleName(StringRef name) {
-  if (!name.startswith("_Z")) {
-    return name;
-  }
-
-  int status = 0;
-  char *demangled = itaniumDemangle(name.data(), nullptr, nullptr, &status);
-  if (status != 0) {
-    return name;
-  }
-  std::string result = demangled;
-  free(demangled);
-  return result;
-}
-
 struct Demangle : public FunctionPass {
   static char ID;
   Demangle() : FunctionPass(ID) {}
 
   bool runOnFunction(Function &F) override {
-    auto name = DemangleName(F.getName());
-    errs() << "Hello: ";
+    auto name = F.getName();
     errs().write_escaped(name) << '\n';
+
+    if (!name.startswith("_Z")) {
+      errs() << "\tskipping non-mangled name\n";
+      return false;
+    }
+
+    ItaniumPartialDemangler Demangler;
+    assert(!Demangler.partialDemangle(name.data()));
+
+    errs() << "\tisData=" << Demangler.isData() << '\n';
+    errs() << "\tisFunction=" << Demangler.isFunction() << '\n';
+    errs() << "\tisCtorOrDtor=" << Demangler.isCtorOrDtor() << '\n';
+    errs() << "\tisSpecialName=" << Demangler.isSpecialName() << '\n';
+
+    if (!Demangler.isFunction()) {
+      return false;
+    }
+
+    size_t size = 1;
+    char *buf = static_cast<char *>(std::malloc(size));
+    assert(buf);
+
+    buf = Demangler.getFunctionName(buf, &size);
+    errs() << "\tFunctionName = " << buf << '\n';
+
+    buf = Demangler.getFunctionDeclContextName(buf, &size);
+    errs() << "\tDeclContext = " << buf << '\n';
+
+    buf = Demangler.getFunctionBaseName(buf, &size);
+    errs() << "\tBaseName = " << buf << '\n';
+
+    buf = Demangler.getFunctionParameters(buf, &size);
+    errs() << "\tParameters = " << buf << '\n';
+
+    buf = Demangler.getFunctionReturnType(buf, &size);
+    errs() << "\tReturnType = " << buf << '\n';
+
+    std::free(buf);
     return false;
   }
 };
